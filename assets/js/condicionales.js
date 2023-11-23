0841ab90fc07e1e9cf81f6798610de7ee910e62c
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2023-11-23 10:54:32 -0300

    add: iconos

--- assets/js/condicionales.js
+++ assets/js/condicionales.js
@@ -5,5 +5,6 @@ var miImagen = document.getElementById("miImagen");
 // Agrega un evento clic a la imagen
 miImagen.addEventListener("click", function() {
     // Toggle (agregar o quitar) la clase 'borde-activo'
+    //Activa o desactiva una clase.
     this.classList.toggle("borde-activo");
 });
